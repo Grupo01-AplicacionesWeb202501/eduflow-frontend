2c235fc3bfde58a542c0a42c1a6c50820fefbf3d
RodMiraval <u202311082@upc.edu.pe>
2025-05-16 01:25:55 -0500

    Update NotificationList.vue

--- src/notification/NotificationList.vue
+++ src/notification/NotificationList.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    <h2>Lista de Notificaciones</h2>
-    <ul>
-      <li v-for="(notification, index) in notifications" :key="index">
-        {{ notification }}
-      </li>
-    </ul>
-  </div>
+  <ul class="notification-list">
+    <li v-for="(notification, index) in notifications" :key="index" class="notification-item">
+      <span class="dot">🔔</span>
+      {{ notification }}
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -15,7 +13,30 @@ export default {
   data() {
     return {
       notifications: ['Notificación 1', 'Notificación 2', 'Notificación 3']
-    };
+    }
   }
-};
+}
 </script>
+
+<style scoped>
+.notification-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.notification-item {
+  background-color: #3b5998;
+  padding: 12px 16px;
+  border-radius: 8px;
+  margin-bottom: 10px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.dot {
+  font-size: 18px;
+}
+</style>

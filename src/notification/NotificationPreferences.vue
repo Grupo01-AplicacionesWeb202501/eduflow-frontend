75ead20c9d95ad043b6859d04cdfe75992dcf9db
RodMiraval <u202311082@upc.edu.pe>
2025-05-15 22:38:09 -0500

    Update and rename temp to NotificationPreferences.vue

---
+++ src/notification/NotificationPreferences.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Preferencias de Notificación</h2>
+    <label>
+      <input type="checkbox" v-model="emailEnabled" />
+      Recibir notificaciones por correo
+    </label>
+    <br />
+    <label>
+      <input type="checkbox" v-model="smsEnabled" />
+      Recibir notificaciones por SMS
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotificationPreferences',
+  data() {
+    return {
+      emailEnabled: true,
+      smsEnabled: false
+    };
+  }
+};
+</script>

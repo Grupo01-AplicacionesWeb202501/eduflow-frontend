150e3ff8607ff151c2521ed80119bd82b67cccd1
RodMiraval <u202311082@upc.edu.pe>
2025-05-15 22:49:56 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const SignInComponent = () => import("../eduflow/pages/sign-in.component.vue");
 const SignUpComponent = () => import("../eduflow/pages/sign-up.component.vue");
+const NotificationPage = () => import("../pages/NotificationPage.vue");
 const PageNotFoundComponent = () => import("../public/pages/page-not-found.component.vue");
 
 const routes = [
-    {path: "/sign-in", name: "SignInComponent", component: SignInComponent},
-    {path: "/sign-up", name: "SignUpComponent", component: SignUpComponent},
-    {path: "/",      name:'default', redirect:{name: "SignInComponent"}},
-    {path: "/:pathMatch(.*)*", name: "PageNotFoundComponent", component: PageNotFoundComponent}
-]
+  { path: "/sign-in", name: "SignInComponent", component: SignInComponent },
+  { path: "/sign-up", name: "SignUpComponent", component: SignUpComponent },
+  { path: "/notificaciones", name: "NotificationPage", component: NotificationPage },
+  { path: "/", name: "default", redirect: { name: "SignInComponent" } },
+  { path: "/:pathMatch(.*)*", name: "PageNotFoundComponent", component: PageNotFoundComponent }
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: routes
-})
+  history: createWebHistory(),
+  routes: routes
+});
 
 export default router;

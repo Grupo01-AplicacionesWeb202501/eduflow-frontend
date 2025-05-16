487f0a8399e2b4cce12178afa18989088fb7cd3c
RodMiraval <u202311082@upc.edu.pe>
2025-05-15 23:51:03 -0500

    Create sign-in.component.vue

---
+++ src/pages/sign-in.component.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Página de Iniciar Sesión</h1>
+    <p>Esta es una vista de prueba para /sign-in</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignInComponent"
+};
+</script>

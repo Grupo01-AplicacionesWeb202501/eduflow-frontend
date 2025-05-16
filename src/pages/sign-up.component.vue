c3965089cd0c174b424b6c31f889903b072f903b
RodMiraval <u202311082@upc.edu.pe>
2025-05-16 00:06:11 -0500

    Create sign-up.component.vue

---
+++ src/pages/sign-up.component.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Registro</h1>
+    <p>Esta es una vista de prueba para /sign-up</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUpComponent"
+};
+</script>

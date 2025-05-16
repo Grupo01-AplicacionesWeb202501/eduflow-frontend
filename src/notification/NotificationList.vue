25444a94f33effb5f5f0453708a09dac797f5716
RodMiraval <u202311082@upc.edu.pe>
2025-05-15 22:37:23 -0500

    Create NotificationList.vue

---
+++ src/notification/NotificationList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Lista de Notificaciones</h2>
+    <ul>
+      <li v-for="(notification, index) in notifications" :key="index">
+        {{ notification }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotificationList',
+  data() {
+    return {
+      notifications: ['Notificación 1', 'Notificación 2', 'Notificación 3']
+    };
+  }
+};
+</script>

d3ea1eb92edf1b57d94e3b837633ca12da730cd3
RodMiraval <u202311082@upc.edu.pe>
2025-05-15 22:43:08 -0500

    Create NotificationPage.vue

---
+++ src/pages/NotificationPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Sistema de Notificaciones</h1>
+    <NotificationList />
+    <NotificationPreferences />
+  </div>
+</template>
+
+<script>
+import NotificationList from '../notification/NotificationList.vue';
+import NotificationPreferences from '../notification/NotificationPreferences.vue';
+
+export default {
+  name: 'NotificationPage',
+  components: {
+    NotificationList,
+    NotificationPreferences
+  }
+};
+</script>

7a36a0cdf1c65268351c43e749579a1d46834f1c
RnArauj0 <u202113612@upc.edu.pe>
2025-05-29 13:58:31 -0500

    feat(main): merge branch "courseManagement"

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import {createRouter, createWebHistory} from "vue-router";
+import CourseManagement from "../views/CourseManagement.vue";
 const LoginComponent = () => import("../eduflow/pages/login.component.vue");
 const SignInStudentComponent = () => import("../eduflow/pages/sign-in-student.component.vue");
 const SignUpStudentComponent = () => import("../eduflow/pages/sign-up-student.component.vue");
-import { createRouter, createWebHistory } from "vue-router";
-import CourseManagement from "../views/CourseManagement.vue";
+
 const PageNotFoundComponent = () => import("../public/pages/page-not-found.component.vue");
 const SignInTeacherComponent = () => import("../eduflow/pages/sign-in-teacher.component.vue");
 const SignUpTeacherComponent = () => import("../eduflow/pages/sign-up-teacher.component.vue");
@@ -15,8 +15,7 @@ const routes = [
     { path: "/sign-in-teacher", name: "SignInTeacherComponent", component: SignInTeacherComponent},
     { path: "/sign-up-teacher", name: "SignUpTeacherComponent", component: SignUpTeacherComponent},
     { path: "/",      name:'default', redirect:{name: "LoginComponents"}},
-    { path: "/:pathMatch(.*)*", name: "PageNotFoundComponent", component: PageNotFoundComponent}
-]
+    { path: "/:pathMatch(.*)*", name: "PageNotFoundComponent", component: PageNotFoundComponent},
     { path: "/courses", name: "CourseManagement", component: CourseManagement },
     { path: "/", name: "default", redirect: { name: "CourseManagement" } },
     { path: "/:pathMatch(.*)*", name: "PageNotFoundComponent", component: PageNotFoundComponent }

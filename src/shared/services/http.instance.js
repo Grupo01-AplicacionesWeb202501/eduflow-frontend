a2627bb69269ec3f06a374ecd0e4f1dc5ee15d3f
KevCast1604 <u202318814@upc.edu.pe>
2025-06-19 17:38:30 -0500

    2nd version

--- src/shared/services/http.instance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const httpInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {'content-type': 'application/json'}
-})
-
-export default httpInstance;
